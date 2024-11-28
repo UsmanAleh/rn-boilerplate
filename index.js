73cc9dc7f4673f6f62a4ba5b6fa615e0d3bed464
UsmanAleh <usman@aleh.tech>
2024-11-28 20:03:50 +0500

    chore(config): ignore deprecated warnings

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
 
+import './injections';
+
 import { name as appName } from './app.json';
 import App from './src/App';
 
--- index.web.js
+++ index.web.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
 
+import './injections';
+
 import { name as appName } from './app.json';
 import App from './src/App';
 
@@ -8,4 +10,3 @@ AppRegistry.runApplication(appName, {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
 });
-
