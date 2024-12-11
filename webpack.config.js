e3d3971bfaf4af3b56066e287b50cf50387ebfc1
israromar <iisraromar@gmail.com>
2024-12-11 20:05:49 +0500

    add react-native-web-linear-gradient support in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -114,6 +114,7 @@ module.exports = {
     ],
     alias: {
       'react-native$': 'react-native-web',
+      'react-native-linear-gradient': 'react-native-web-linear-gradient',
     },
   },
   module: {
