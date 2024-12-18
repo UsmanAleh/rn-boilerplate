2c749ae76ae26ca5945b273f26503adb1842ce75
israromar <iisraromar@gmail.com>
2024-12-18 14:37:19 +0500

    add button and gradient border components

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,6 @@
 export { default as AssetByVariant } from './AssetByVariant/AssetByVariant';
+export { default as Button } from './Button/Button';
+export { default as GradientBorder } from './GradientBorder/GradientBorder';
 export { default as GradientText } from './GradientText/GradientText';
 export { default as IconByVariant } from './IconByVariant/IconByVariant';
 export { default as Skeleton } from './Skeleton/Skeleton';
